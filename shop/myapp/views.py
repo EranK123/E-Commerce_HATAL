5394f8898f1c0a34ad89cbda79bc80b641d91de0
erankatz <eranki.katz@gmail.com>
2023-11-20 10:01:01 +0200

    added some improvements

--- shop/myapp/views.py
+++ shop/myapp/views.py
@@ -12,15 +12,35 @@ def home(request):
 def signup(request):
     if request.method == "POST":
         username = request.POST['username']
-        fname = request.POST['fname']
-        lname = request.POST['lname']
+        # fname = request.POST['fname']
+        # lname = request.POST['lname']
         email = request.POST['email']
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
 
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exist! Please try some other username.")
+            return redirect('home')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email Already Registered!!")
+            return redirect('home')
+
+        if len(username) > 20:
+            messages.error(request, "Username must be under 20 charcters!!")
+            return redirect('home')
+
+        if pass1 != pass2:
+            messages.error(request, "Passwords didn't matched!!")
+            return redirect('home')
+
+        if not username.isalnum():
+            messages.error(request, "Username must be Alpha-Numeric!!")
+            return redirect('home')
+
         myuser = User.objects.create_user(username, email, pass1)
-        myuser.first_name = fname
-        myuser.last_name = lname
+        # myuser.first_name = fname
+        # myuser.last_name = lname
         myuser.save()
 
         messages.success(request, "Your Account has been created successfully")
